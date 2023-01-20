e6df7b30139d1b5d2a32001c2d33f79df5d7f1c2
Pratik Narendra Gupta <pratikngupta@outlook.com>
2023-01-20 09:09:14 -0500

    Added lab 2

---
+++ src/Lab/pgupta85_Lab2/src/Q1/Pair.java
@@ -0,0 +1,39 @@
+/*************************************************************************************************************************
+ * Name: Pratik Narendra Gupta
+ * Student ID: 251211859
+ * Date: 1st October
+ * Task: Create generic class Pair which will take two generic type of data and will return the data in form of pair.
+ *************************************************************************************************************************/
+
+package Q1;
+
+public class Pair<Y,N> {
+    private Y key;
+    private N value;
+
+    //Creating constructor of Pair class, which will accept two generic type of data
+    public Pair(Y key, N value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    //setter method for key
+    public void setKey(Y key) {
+        this.key = key;
+    }
+
+    //getter method for key
+    public void setValue(N value) {
+        this.value = value;
+    }
+
+    //getter method for value
+    public N getValue() {
+        return value;
+    }
+
+    //getter method for key
+    public Y getKey() {
+        return key;
+    }
+}
